1aaeb44f6ce0aafd3a565a81daca13c4b963490f
Lenanetka <8882845+Lenanetka@users.noreply.github.com>
2025-07-08 21:28:56 +0200

    Adjust card position

--- src/routes/cards/+page.svelte
+++ src/routes/cards/+page.svelte
@@ -23,12 +23,13 @@
   }
 </script>
 
-
-<div class="flex justify-center min-h-screen gap-4 p-4">
-  <div>
-    <Card />
-  </div>
-  <div>
-    <WordsTable words={words} saveWord={saveWord} deleteWord={deleteWord} />
+<div class="flex flex-col items-center min-h-screen gap-4 p-4">
+  <div class="flex-col min-h-screen gap-4 p-4">
+      <div class="flex justify-center mb-4">
+        <Card />
+      </div>
+      <div>
+        <WordsTable words={words} saveWord={saveWord} deleteWord={deleteWord}/>
+      </div>
   </div>
 </div>
