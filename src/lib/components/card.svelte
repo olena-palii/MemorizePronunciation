0180bed38824684b276a84bc16c18103f18e41fc
Lenanetka <8882845+Lenanetka@users.noreply.github.com>
2025-07-11 19:51:46 +0200

    Display first word on card

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+    import { Word } from "$lib";
+    import { playNativeAudio } from "$lib";
+
+    interface Props {
+        word: Word;
+    }
+
+    let { word = $bindable()}: Props = $props();
+</script>
+
 <div class="card w-96 bg-base-100 shadow-sm">
   <div class="card-body">
     <div class="flex justify-between">
@@ -6,7 +17,7 @@
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
         </svg>
       </button>
-      <h2 class="text-xl font-bold">word</h2>
+      <h2 class="text-xl font-bold">{word.word}</h2>
       <button aria-label="Next" class="btn btn-square btn-sm">
         <svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
@@ -14,24 +25,28 @@
       </button>
     </div>
     <div class="flex justify-center gap-8 mt-8">
-      <button class="btn btn-circle btn-success btn-xl" aria-label="Listen to pronunciation" data-tip>
+      <button class="btn btn-circle btn-success btn-xl" aria-label="Listen to pronunciation" onclick={() => playNativeAudio(word.word)}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
         </svg>
       </button>
-      <button class="btn btn-circle btn-error btn-xl" aria-label="Record your pronunciation" data-tip>
+      <button class="btn btn-circle btn-error btn-xl" aria-label="Record your pronunciation">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
         </svg>
       </button>
-      <button class="btn btn-circle btn-warning btn-xl" aria-label="Record your pronunciation" data-tip>
+      <button class="btn btn-circle btn-warning btn-xl" aria-label="Record your pronunciation">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
         </svg>
       </button>
     </div>
     <div class="mt-6">
-      <button class="btn btn-block btn-success">Mark as learned</button>
+      {#if word.isLearned}
+        <button class="btn btn-block btn-error">Reset learning</button>
+      {:else}
+        <button class="btn btn-block btn-success">Mark as known</button>
+      {/if}
     </div>
   </div>
 </div>
--- src/routes/cards/+page.svelte
+++ src/routes/cards/+page.svelte
@@ -3,6 +3,7 @@
   import { onMount } from "svelte";
   import { Word, apiWords, WordsTable, Card } from "$lib";
   let words: Word[] = [];
+  let selectedWord: Word;
 
   async function refreshTable() {
     words = await apiWords.getWords();
@@ -10,6 +11,7 @@
 
   onMount(async () => {
     await refreshTable();
+    selectedWord = words[0] || new Word({ word: "N/A" });
   });
 
   async function saveWord(word: Word) {
@@ -23,13 +25,19 @@
   }
 </script>
 
-<div class="flex flex-col items-center min-h-screen gap-4 p-4">
-  <div class="flex-col min-h-screen gap-4 p-4">
-      <div class="flex justify-center mb-4">
-        <Card />
-      </div>
-      <div>
-        <WordsTable words={words} saveWord={saveWord} deleteWord={deleteWord}/>
-      </div>
+{#if words && selectedWord}
+  <div class="flex flex-col items-center min-h-screen gap-4 p-4">
+    <div class="flex-col min-h-screen gap-4 p-4">
+        <div class="flex justify-center mb-4">
+          <Card bind:word={selectedWord}/>
+        </div>
+        <div>
+          <WordsTable words={words} saveWord={saveWord} deleteWord={deleteWord}/>
+        </div>
+    </div>
   </div>
-</div>
+  {:else}
+    <div class="flex justify-center items-center min-h-screen"> 
+      <span class="loading loading-spinner loading-xl"></span>
+    </div>
+  {/if}
