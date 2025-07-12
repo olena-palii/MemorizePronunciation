7e5d4eccdb27d199e2df345ba257cad2997c8720
Lenanetka <8882845+Lenanetka@users.noreply.github.com>
2025-07-12 17:43:32 +0200

    Disable play button if ths word is not recorded

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -35,6 +35,28 @@
     };
   });
 
+  let recording = $state<boolean>(false);
+  let recordedWord = $state<Word | null>(null);
+  let recordedAudio: Blob;
+
+  async function record() {
+    recording = true;
+    await startRecordingAudio();
+    setTimeout(() => { stop(); }, 10000);
+  }
+
+  async function stop() {
+    recordedAudio = await stopRecordingAudio();
+    recording = false;
+    recordedWord = word;
+  }
+
+  function isRecorded(): boolean {
+    if(!recordedWord) return false;
+    if (recordedWord.id !== word.id) return false;
+    return true;
+  }
+
   async function markAsLearned() {
     word.markAsLearned();
     updateWord(word);
@@ -45,20 +67,6 @@
     word.resetLearning();
     updateWord(word);
     next();
-  }
-
-  let recording = $state<boolean>(false);
-  let blob: any;
-  
-  async function record() {
-    recording = true;
-    await startRecordingAudio();
-    setTimeout(() => { stop(); }, 10000);
-  }
-
-  async function stop() {
-    blob = await stopRecordingAudio();
-    recording = false;
   }
 </script>
 
@@ -84,7 +92,7 @@
           </svg>
         </button>
       {/if}
-      <button class="play btn btn-circle btn-warning btn-xl" aria-label="Play recorded pronunciation" onclick={ () => blob ? playRecordedAudio(blob) : null }>
+      <button class="play btn btn-circle btn-warning btn-xl" aria-label="Play recorded pronunciation" onclick={ () => playRecordedAudio(recordedAudio) } disabled={!isRecorded()}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
         </svg>
