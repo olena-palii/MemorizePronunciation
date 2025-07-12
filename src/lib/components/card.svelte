658889e4ce20aab44c5de42f45d2bf8bc05e72fe
Lenanetka <8882845+Lenanetka@users.noreply.github.com>
2025-07-12 18:09:30 +0200

    Change recording buttons colors

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -86,13 +86,13 @@
           </svg>
         </button>
       {:else}
-        <button class="record btn btn-circle btn-error btn-xl" aria-label="Record your pronunciation" onclick={record}>
+        <button class="record btn btn-circle btn-info btn-xl" aria-label="Record your pronunciation" onclick={record}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
           </svg>
         </button>
       {/if}
-      <button class="play btn btn-circle btn-warning btn-xl" aria-label="Play recorded pronunciation" onclick={ () => playRecordedAudio(recordedAudio) } disabled={!isRecorded()}>
+      <button class="play btn btn-circle btn-info btn-xl" aria-label="Play recorded pronunciation" onclick={ () => playRecordedAudio(recordedAudio) } disabled={!isRecorded()}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
         </svg>
