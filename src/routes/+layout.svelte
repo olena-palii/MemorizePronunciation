b82c4efcecf3adb9f212fe6ac63a45f939abedbd
Lenanetka <8882845+Lenanetka@users.noreply.github.com>
2025-07-13 14:59:08 +0200

    Add theme toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import '../app.css';
+  import { Theme } from '$lib'
 </script>
 
 <div class="navbar bg-base-100 shadow-sm fixed top-0 left-0 w-full z-50">
   <div class="flex-1">
-    <a href="/" class="btn btn-ghost text-xl">Memorize Pronunciation</a>
+    <a href="/" class="btn btn-ghost text-xl">Memorize Pr.</a>
   </div>
-  <div class="navbar-end hidden lg:flex">
+  <div class="navbar-end">
     <ul class="menu menu-horizontal px-1">
-      <li>
+      <li class="hidden lg:flex">
           <a href="/">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -16,7 +17,7 @@
               Home
           </a>
       </li>
-      <li>
+      <li class="hidden lg:flex">
           <a href="/cards">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
@@ -24,13 +25,16 @@
               Cards
           </a>
       </li>
-      <li>
+      <li class="hidden lg:flex">
           <a href="/create">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
               </svg>
               Create
           </a>
+      </li>
+      <li>
+          <Theme />
       </li>
     </ul>
   </div>
