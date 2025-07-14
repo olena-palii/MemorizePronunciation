08f6d120d9c5c92ca3312cfde7e98c8331a7efce
Olena Palii <8882845+olena-palii@users.noreply.github.com>
2025-07-14 18:48:58 +0200

    Update formatting of lib export file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,25 @@
 // Copyright 2025 Olena Palii
 // SPDX-License-Identifier: Apache-2.0
 
-// place files you want to import through the `$lib` alias in this folder.
+// Place files you want to import through the `$lib` alias in this folder.
+
+// Types
 export { Word } from "./word";
 export type { WordDto, SaveStatisticsDto, DeleteStatisticsDto } from "./dto";
+
+// API
 export * as apiWords from "./api/api-words";
+
+// Utils
 export { textToSpeech, startRecordingAudio, stopRecordingAudio, playRecordedAudio } from "./speech";
+
+// Components
 export { default as WordsTable } from './components/words-table.svelte';
 export { default as Card } from './components/card.svelte';
 export { default as AddWord } from './components/add-word.svelte';
 export { default as Theme } from './components/theme.svelte';
+
+// Icons
 export { default as NavHomeIcon } from './icons/nav-home.svelte';
 export { default as NavCardsIcon } from './icons/nav-cards.svelte';
 export { default as NavCreateIcon } from './icons/nav-create.svelte';
