ab72c06cde7bfc2b30631d65717e79e58eb354c5
Olena Palii <8882845+olena-palii@users.noreply.github.com>
2025-07-15 22:27:30 +0200

    Highlight selected page in navbar and dock

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,21 @@
 
 <script lang="ts">
   import "../app.css";
+  import { page } from '$app/state';
   import { Theme, NavHomeIcon, NavCardsIcon, NavCreateIcon } from "$lib";
   const navItems = [
     { href: "/", icon: NavHomeIcon, label: "Home" },
     { href: "/cards", icon: NavCardsIcon, label: "Cards" },
     { href: "/create", icon: NavCreateIcon, label: "Create" },
   ];
+
+  function dockActivePage(href: string): string {
+    return page.url.pathname === href ? 'dock-active' : '';
+  }
+
+  function navActivePage(href: string): string {
+    return page.url.pathname === href ? 'text-gray-300' : '';
+  }
 </script>
 
 <nav class="navbar z-50 bg-base-100 shadow-sm fixed top-0" aria-label="Main navigation">
@@ -20,7 +29,7 @@
     <ul class="menu menu-horizontal px-1">
       {#each navItems as item}
         <li class="hidden lg:flex">
-          <a href={item.href}>
+          <a href={item.href} class={navActivePage(item.href)}>
             <svelte:component this={item.icon} />
             {item.label}
           </a>
@@ -35,7 +44,7 @@
 
 <nav class="dock z-50 lg:hidden fixed bottom-0 h-16" aria-label="Mobile navigation">
   {#each navItems as item}
-    <a href={item.href}>
+    <a href={item.href} class="{dockActivePage(item.href)}">
       <svelte:component this={item.icon} />
       <span class="dock-label">{item.label}</span>
     </a>
