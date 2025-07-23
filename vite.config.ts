073fdb8df1921e45500a8036093d8878d4fda7cd
Olena Palii <8882845+olena-palii@users.noreply.github.com>
2025-07-23 19:18:28 +0200

    Use http instead of https for local development

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
-import fs from 'fs';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import { certExpiresSoon, generateCert, keyPath, certPath } from './certificate.js';
-
-const certExists = fs.existsSync(certPath) && fs.existsSync(keyPath);
-if (!certExists || certExpiresSoon(certPath)) generateCert();
 
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 	server: {
 		host: 'localhost',
 		port: 5173,
-		https: {
-			key: fs.readFileSync(keyPath),
-			cert: fs.readFileSync(certPath)
-		}
 	},
 	test: {
 		projects: [
