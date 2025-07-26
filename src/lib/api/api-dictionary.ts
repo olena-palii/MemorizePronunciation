06257ac296b53241755984a2ac39cb9cda4b9550
Olena Palii <8882845+olena-palii@users.noreply.github.com>
2025-07-26 23:11:31 +0200

    Error message for api dictionary

--- src/lib/api/api-dictionary.ts
+++ src/lib/api/api-dictionary.ts
@@ -1,7 +1,8 @@
 // Copyright 2025 Olena Palii
 // SPDX-License-Identifier: Apache-2.0
 
-import type{ DictionaryDto } from '$lib';
+import type { DictionaryDto } from '$lib';
+import { addToast } from '$lib';
 
 export async function getDefinition(word: string): Promise<DictionaryDto[]> {
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
@@ -9,6 +10,7 @@ export async function getDefinition(word: string): Promise<DictionaryDto[]> {
         const json = await response.json();
         return json as DictionaryDto[];
     }
+    if (response.status === 404) return [];
+    addToast({ message: "Failed to fetch definition", type: 'error' });
     return [];
 }
-    
